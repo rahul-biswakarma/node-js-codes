a1f68566fe06401adc4f14958cc679a8a93a4041
Rahul Biswakarma <rahul.id39@gmail.com>
2022-09-16 01:45:37 +0530

    Added routing

--- web_server/index.js
+++ web_server/index.js
@@ -2,8 +2,16 @@ import { createServer } from "http";
 
 const PORT = 3000;
 const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home");
+  } else if (req.url == "/hello") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
